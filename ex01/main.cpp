5ddc89ad6664786554bc24cfe7edaa6386d51c28
Natália Medeiros Da Silva De Almeida <natalia@MacBookPro.home>
2024-11-05 17:37:40 +0100

    update

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,11 +6,11 @@
 /*   By: natalia <natalia@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/11/04 18:40:21 by natalia       #+#    #+#                 */
-/*   Updated: 2024/11/04 18:42:33 by natalia       ########   odam.nl         */
+/*   Updated: 2024/11/05 17:37:13 by natalia       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.cpp"
+#include "Zombie.hpp"
 
 int	main(void)
 {
