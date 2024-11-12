ef1969ad4453f8fd8430c3c482ee115e23a836cc
Natália Medeiros Da Silva De Almeida <natalia@MacBookPro.home>
2024-11-12 16:00:40 +0100

    update strptr

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: natalia <natalia@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/11/05 17:34:38 by natalia       #+#    #+#                 */
-/*   Updated: 2024/11/08 13:41:15 by natalia       ########   odam.nl         */
+/*   Updated: 2024/11/12 16:00:04 by natalia       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(void)
 	std::cout << "The memory address held by stringPTR is: " << strPTR << std::endl;
 	std::cout << "The memory address held by stringREF is: " << &strREF << std::endl;
 	std::cout << "The value of the string variable is: " << str << std::endl;
-	std::cout << "The value pointed to by stringPTR: " << strPTR << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *strPTR << std::endl;
 	std::cout << "The value pointed to by stringREF: " << strREF << std::endl;
 
 	return (0);
